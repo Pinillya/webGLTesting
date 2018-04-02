f87ce5470fea68b476f6042f2a37ad3d896ae5d6
Pinillya <Pinillya@hotmail.com>
2018-04-02 13:05:11 +0200

    Basic shader implementation

--- TriangleApp.js
+++ TriangleApp.js
@@ -1,7 +1,28 @@
 var vertexShaderText =
 [
-'precision mediump float;'
-].join('\n')
+
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'',
+'void main()',
+'{',
+' gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}',
+''
+
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'void main()',
+'{',
+' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+'}',
+''
+].join('\n');
 
 var initTriangle = function () {
 	console.log('init Triangle ');
@@ -23,7 +44,38 @@ var initTriangle = function () {
 	gl.clearColor(0.75, 0.85, 0.8, 1.0); // setting the color, but does not render at this point
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //this is where we render it.
 
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
 
+	gl.shaderSource(vertexShader, vertexShaderText);
+	gl.shaderSource(fragShader, fragmentShaderText);
+
+	gl.compileShader(vertexShader);
+	if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vert shader ', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
+
+	gl.compileShader(fragShader);
+	if(!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling frag shader ', gl.getShaderInfoLog(fragShader));
+		return;
+	}
+
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragShader);
+	gl.linkProgram(program);
+	if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		console.error('ERROR linking program ', gl.getProgramInfoLog(program));
+		return;
+	}
+
+	gl.validateProgram(program);
+	if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS) ) {
+		console.error('ERROR linking program ', gl.getProgramInfoLog(program));
+		return;
+	}
 
 }
